70b4c9fbe3e92405117d742c8c469059b8c770fa
DeeMATT <mattode@outlook.com>
2019-10-14 18:11:38 +0100

    Product catalog

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200,
@@ -10,6 +12,10 @@ class Category(models.Model):
         ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                        args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -25,7 +31,7 @@ class Product(models.Model):
                               blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
+    stock = models.PositiveSmallIntegerField(default=0)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -33,6 +39,10 @@ class Product(models.Model):
     class Meta:
         ordering = ('name',)
         index_together = (('id', 'slug'),)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                        args=[self.id, self.slug])
 
     def __str__(self):
         return self.name
