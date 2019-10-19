decd102a54083de868f3258a33b8010970b70e86
DeeMATT <mattode@outlook.com>
2019-10-19 17:35:54 +0100

    Generating and rendering pdf invoices dynamically

--- orders/admin.py
+++ orders/admin.py
@@ -33,7 +33,11 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 def order_detail(obj):
-    return mark_safe("<a href='{}'>View</a>".format(reverse('orders:admin_order_detail', args=[obj.id])))
+    return mark_safe(f"<a href='{reverse('orders:admin_order_detail', args=[obj.id])}'>View</a>")
+
+def order_pdf(obj):
+    return mark_safe(f"<a href='{reverse('orders:admin_order_pdf', args=[obj.id])}'>PDF</a>")
+order_pdf.short_description = 'Invoice'
 
 
 @admin.register(Order)
@@ -41,7 +45,8 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
                     'created', 'updated',
-                    order_detail]
+                    order_detail,
+                    order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
