e0ccfbb12a460dc1d4957a16b6ff835cbbbbb588
DeeMATT <mattode@outlook.com>
2019-10-19 19:52:17 +0100

    Generate csv files and pdf invoice for orders dynamically

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,12 @@
 import braintree
 from django.shortcuts import render, redirect, get_object_or_404
 from orders.models import Order
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.core.mail import EmailMessage
+import weasyprint
+from io import BytesIO
+
 
 def payment_process(request):
     order_id = request.session.get('order_id')
@@ -18,11 +24,35 @@ def payment_process(request):
             }
         })
         if result.is_success:
+            
             # mark the order as paid
             order.paid = True
+            
             # store the unique transaction id
             order.braintree_id = result.transaction.id
             order.save()
+            
+            # create invoice e-mail
+            subject = f'My Shop - Invoice no. {order.id}'
+            message = 'Please, find attached the invoice for your recent\
+                purchase.'
+            email = EmailMessage(subject,
+                                message,
+                                from_email='admin@myshop.com',
+                                to=[order.email])
+            
+            # generate PDF
+            html = render_to_string('orders/order/pdf.html', {'order': order})
+            out = BytesIO()
+            stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+            weasyprint.HTML(string=html).write_pdf(out,stylesheets=stylesheets)
+            
+            # attach PDF file
+            email.attach(f'order_{order.id}', out.getvalue(), 'application/pdf')
+            
+            # send e-mail
+            email.send()
+
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
